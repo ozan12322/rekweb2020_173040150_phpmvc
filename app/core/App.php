e5af6fa2136741b03930cf0a3f4e12e2e1793eea
Ozan <bakonong99@gmail.com>
2019-10-07 02:09:45 +0700

    Menambahkan fungsi Routing

--- app/core/App.php
+++ app/core/App.php
@@ -1,7 +1,19 @@
 <?php 
 
 	class App {
+
 		public function __construct(){
-			echo "Hello";
+			$url = $this->parseUrl();
+			var_dump($url);
 		}
+
+		public function parseUrl(){
+			if( isset($_GET['url']) ){
+				$url = rtrim($_GET['url'], '/');
+				$url = filter_var($url, FILTER_SANITIZE_URL);
+				$url = explode('/', $url);
+				return $url;
+			}
+		}
+
 	}
