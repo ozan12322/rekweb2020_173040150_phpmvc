ac7785c559748febcf16ebb7734d76840efeee00
Ozan <bakonong99@gmail.com>
2019-10-08 00:50:31 +0700

    Menambahkan fungsi hapus data mahasiswa

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -28,4 +28,15 @@
 				exit;
 			}
 		}
+		public function hapus($id){
+			if($this->model('Mahasiswa_model')->hapusDataMahasiswa($id) > 0) {
+				Flasher::setFlash('berhasil' , 'dihapus', 'success');
+				header('Location: ' . BASEURL . '/mahasiswa');
+				exit;
+			} else {
+				Flasher::setFlash('gagal' , 'dihapus', 'danger');
+				header('Location: ' . BASEURL . '/mahasiswa');
+				exit;
+			}
+		}
 	}
